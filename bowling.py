79de67e935b3839094b0219f4b924be47bff921e
Lakos Nikolett <lakosnikolett@gmail.com>
2017-12-01 09:00:32 +0100

    If statements are separated for better view.

--- bowling.py
+++ bowling.py
@@ -7,25 +7,32 @@ def score(game):
             result += 10 - last
         else:
             result += get_value(game[i])
+
         if frame < 10 and get_value(game[i]) == 10:
             if game[i] == '/':
                 result += get_value(game[i+1])
             elif game[i].lower() == "x":
                 result += get_value(game[i+1])
+
                 if game[i+2] == '/':
                     result += 10 - get_value(game[i+1])
                 else:
                     result += get_value(game[i+2])
+
         last = get_value(game[i])
+
         if not in_first_half:
             frame += 1
+
         if in_first_half is True:
             in_first_half = False
         else:
             in_first_half = True
+
         if game[i].lower() == 'x':
             in_first_half = True
             frame += 1
+            
     return result
 
 
