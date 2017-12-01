fc64ec06b4e7086305f9f263a9ba321aebe0e936
Lakos Nikolett <lakosnikolett@gmail.com>
2017-12-01 08:51:13 +0100

    get_value function is shortened.

--- bowling.py
+++ bowling.py
@@ -30,13 +30,9 @@ def score(game):
 
 
 def get_value(char):
-    if char == '1' or char == '2' or char == '3' or \
-       char == '4' or char == '5' or char == '6' or \
-       char == '7' or char == '8' or char == '9':
+    if char in "123456789":
         return int(char)
-    elif char == 'X' or char == 'x':
-        return 10
-    elif char == '/':
+    elif char.lower() == 'x' or char == '/':
         return 10
     elif char == '-':
         return 0
